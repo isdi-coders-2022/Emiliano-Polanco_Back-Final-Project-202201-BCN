572c4bd4dd3f2af0b65ffce352bfcceb81199355
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-07 21:57:55 +0100

    Adding the error controllers to the middleware chain

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const { notFoundError, internalServerError } = require("./middlewares/errors");
 
 const app = express();
 
@@ -10,5 +11,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use(notFoundError);
+app.use(internalServerError);
 
 module.exports = app;
