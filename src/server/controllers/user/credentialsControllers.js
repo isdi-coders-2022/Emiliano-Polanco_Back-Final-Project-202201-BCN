ecbe1ff58a20920f3a6ce3330d7be036cbe1d5d2
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-21 21:03:29 +0100

    Login with google controller and routing the endpoint

--- src/server/controllers/user/credentialsControllers.js
+++ src/server/controllers/user/credentialsControllers.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
+
+const client = new OAuth2Client(
+  "444624699992-6j9qum8qhkajkclj2tc3nnifs0o88gqh.apps.googleusercontent.com"
+);
 const User = require("../../../database/models/User");
 
 const secret = process.env.SECRET;
@@ -30,6 +35,64 @@ const loginController = async (req, res, next) => {
     }
   } else {
     loginErrorProtocol();
+  }
+};
+
+const loginRegisterGoogleController = async (req, res, next) => {
+  const { token } = req.body;
+  if (token) {
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID,
+      });
+
+      const payload = ticket.getPayload();
+
+      const userFounded = await User.findOne({ username: payload.sub });
+      if (userFounded) {
+        const payloadUser = {
+          name: userFounded.name,
+          lastname: userFounded.lastName,
+          username: userFounded.username,
+          // eslint-disable-next-line no-underscore-dangle
+          _id: userFounded._id,
+        };
+        const tokenJWT = jwt.sign(payloadUser, secret);
+        res.status(200).json({ tokenJWT });
+      } else {
+        const userid = payload.sub;
+
+        const newUser = {
+          name: payload.given_name,
+          lastname: payload.family_name,
+          username: userid,
+          email: payload.email,
+          password: "none",
+        };
+
+        const createdUser = await User.create(newUser);
+
+        const payloadUser = {
+          name: createdUser.name,
+          lastname: createdUser.lastname,
+          username: createdUser.username,
+          // eslint-disable-next-line no-underscore-dangle
+          _id: createdUser._id,
+        };
+
+        const tokenJWT = await jwt.sign(payloadUser, secret);
+        res.json({ token: tokenJWT });
+      }
+    } catch (error) {
+      error.status = 401;
+      error.message = "HOly moly that token it's invalid or its caduced";
+      next(error);
+    }
+  } else {
+    const error = new Error("There's no token here");
+    error.status = 401;
+    next(error);
   }
 };
 
@@ -69,4 +132,8 @@ const registerController = async (req, res, next) => {
     res.json({ token });
   }
 };
-module.exports = { loginController, registerController };
+module.exports = {
+  loginController,
+  registerController,
+  loginRegisterGoogleController,
+};
