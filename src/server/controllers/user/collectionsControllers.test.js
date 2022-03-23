191b23ac9bb5c5c91fab2ad479c1ede7eeca3de7
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-23 19:21:02 +0100

    Fixing unitary test

--- src/server/controllers/user/collectionsControllers.test.js
+++ src/server/controllers/user/collectionsControllers.test.js
@@ -1,6 +1,21 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { default: mongoose } = require("mongoose");
+const connectDataBase = require("../../../database");
 const {
   collectionUserSnippetsController,
 } = require("./collectionsControllers");
+
+let mongoServer;
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const connectionString = mongoServer.getUri();
+  await connectDataBase(connectionString);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
 
 describe("Given a createSnippetController", () => {
   describe("When it receives a request with a dangerous payload ", () => {
@@ -9,7 +24,7 @@ describe("Given a createSnippetController", () => {
       const req = {
         userId: new Error(),
       };
-      await collectionUserSnippetsController(req, "res", nextMock);
+      await collectionUserSnippetsController(req, null, nextMock);
 
       expect(nextMock).toHaveBeenCalled();
     });
